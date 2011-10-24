3bef7b538aad1377378b9f0f53c1b2732b11d902
Vit Chmura <xcrook@gmail.com>
2011-10-24 22:58:25 +0200

    zprava 6, pokus o novy branch

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 echo "pica vole pica kunda pes, lokalni zmena, druha lokalni zmena, treti lokalni zmena";
 
-echo "dalsi zmena, pokus o novy branch";
+echo "dalsi zmena, pokus o novy branch alla";
